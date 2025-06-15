9c16ee92ff843aba7eb49c3e5cccd6e6710c2467
Ayyappajith <124246626+AYJ01@users.noreply.github.com>
2025-06-15 18:03:56 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -15,7 +15,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"  crossOrigin={"anonymous"}></link>
     <script src={"https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"} integrity={"sha384-c0ySq6I2MlH5oqwZPxXrrqOd9GZSkXWvVwRCjaGQwN2fnDJY4Dh+G3nGLJrO5L1u"} crossOrigin={"anonymous"}></script>
-{/*     <Hero/> */}
+    <Hero/>
     <About/>
     <Skills/>
     <Projects/>
