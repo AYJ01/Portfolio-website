a14ab254c1993082fcab7497f32d0c91084dece7
Ayyappajith <124246626+AYJ01@users.noreply.github.com>
2025-06-15 12:13:56 +0530

    Update Certifications.jsx

--- src/CERTIFICATIONS/Certifications.jsx
+++ src/CERTIFICATIONS/Certifications.jsx
@@ -10,19 +10,19 @@ export default function Certifications(){
         <div id="certificates">
             <div className="certificate">
                 <div className="certifyimage">
-                    <img src={jshrankbasic} alt="Javascript (Basic)" onClick={()=> {location.href="https://www.hackerrank.com/certificates/ba14b86c438b"}} />
+                    <img src={jshrankbasic} loading="lazy" alt="Javascript (Basic)" onClick={()=> {location.href="https://www.hackerrank.com/certificates/ba14b86c438b"}} />
                 </div>
                 <div style={{textAlign:"left"}}>
-                    <img className="mx-3" src={hrLogo} style={{width:"50px",height:"50px",objectFit:"cover",borderRadius:"100%"}} alt="" />
+                    <img className="mx-3" src={hrLogo} loading="lazy" style={{width:"50px",height:"50px",objectFit:"cover",borderRadius:"100%"}} alt="" />
                     <span className="mx-3" style={{fontSize:"16px"}}>Javascript (Basic)</span>
                 </div>
             </div>
             <div className="certificate">
                 <div className="certifyimage">
-                    <img src={iitPatna} alt="Javascript (Basic)" onClick={()=> {location.href="https://unstop.com/certificate-preview/7be94f67-0700-4407-b8c3-2cc5e9cec0b5?utm_campaign"}} />
+                    <img src={iitPatna} loading="lazy" alt="Javascript (Basic)" onClick={()=> {location.href="https://unstop.com/certificate-preview/7be94f67-0700-4407-b8c3-2cc5e9cec0b5?utm_campaign"}} />
                 </div>
                 <div style={{textAlign:"left"}}>
-                    <img className="mx-3" src={unstopLogo} style={{width:"50px",height:"50px",objectFit:"cover",borderRadius:"100%"}} alt="" />
+                    <img className="mx-3" loading="lazy" src={unstopLogo} style={{width:"50px",height:"50px",objectFit:"cover",borderRadius:"100%"}} alt="" />
                     <span className="mx-3" style={{fontSize:"16px"}}>Frontend Frontier</span>
                     <span className="text-center" style={{fontSize:"16px",display:"block"}}>by IIT Patna via Unstop</span>
                 </div>
